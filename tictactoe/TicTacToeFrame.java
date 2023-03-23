f83ce57d70d3f16a467b309a7759229f0e2616a5
James <jamesruntas@cmail.carleton.ca>
2023-03-23 16:51:42 -0400

    first

---
+++ tictactoe/TicTacToeFrame.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+/**
+ * A class modelling a tic-tac-toe (noughts and crosses, Xs and Os) game in a very
+ * simple GUI window.
+ * 
+ * @author Lynn Marshall
+ * @version November 8, 2012
+ */
+
+public class TicTacToeFrame extends TicTacToe 
+{ 
+   private JTextArea status; // text area to print game status
+   
+   /** 
+    * Constructs a new Tic-Tac-Toe board and sets up the basic
+    * JFrame containing a JTextArea in a JScrollPane GUI.
+    */
+   public TicTacToeFrame()
+   { 
+       // add the necessary code here
+   }
+   
+   /**
+    * Prints the board to the GUI using toString().
+    */
+    public void print() 
+    {  
+        // add code here
+    }
+
+}
